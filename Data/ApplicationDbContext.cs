33bbe5bdb104cd3d9aace4508636bea9c8f33c3f
kkisah <matosisa582@gmail.com>
2025-04-11 14:40:09 -0300

    Css, Cardapio, Privacidade, Home

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,7 +1,5 @@
-﻿using MenuEduca01.Models;
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
-
 
 namespace MenuEduca01.Data
 {
@@ -10,17 +8,6 @@ namespace MenuEduca01.Data
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
-        }
-
-        public DbSet<CadastroUsuario> Usuarios { get; set; }
-        //public DbSet<Cardapio> Cardapios { get; set; }
-
-        protected override void OnModelCreating(ModelBuilder builder)
-        {
-            base.OnModelCreating(builder);
-            builder.Entity<CadastroUsuario>().ToTable("Usuarios");
-
-            //builder.Entity<Cardapio>().ToTable("Cardapios");
         }
     }
 }
