03b156ca1ae118ea02717226c75b6c625ce60b70
soriano1010 <raissasoriano51@gmail.com>
2025-04-11 14:08:46 -0300

    r

--- Data/Migrations/20250411170708_InsercaoMedica.Designer.cs
+++ Data/Migrations/20250411170708_InsercaoMedica.Designer.cs
@@ -4,6 +4,7 @@ using MenuEduca01.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace MenuEduca01.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250411170708_InsercaoMedica")]
+    partial class InsercaoMedica
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
