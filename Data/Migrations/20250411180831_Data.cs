d780309f39d0ac96e919bf529e2615db69f67dfe
soriano1010 <raissasoriano51@gmail.com>
2025-04-11 15:09:28 -0300

    Alteração no campo Data de Cadastro

---
+++ Data/Migrations/20250411180831_Data.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MenuEduca01.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class Data : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DataCadastro",
+                table: "InsercaoMedicas",
+                type: "datetime2",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "DataCadastro",
+                table: "InsercaoMedicas",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+        }
+    }
+}
