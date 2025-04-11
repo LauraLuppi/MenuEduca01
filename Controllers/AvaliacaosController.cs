fe76be5ed027d57df9db2f58ca2f36d5cd7a091e
soriano1010 <raissasoriano51@gmail.com>
2025-04-11 14:11:26 -0300

    r

---
+++ Controllers/AvaliacaosController.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using MenuEduca01.Data;
+using MenuEduca01.Models;
+
+namespace MenuEduca01.Controllers
+{
+    public class AvaliacaosController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AvaliacaosController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Avaliacaos
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Avaliacaos.ToListAsync());
+        }
+
+        // GET: Avaliacaos/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var avaliacao = await _context.Avaliacaos
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (avaliacao == null)
+            {
+                return NotFound();
+            }
+
+            return View(avaliacao);
+        }
+
+        // GET: Avaliacaos/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Avaliacaos/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,UsuariosId,CardapioId")] Avaliacao avaliacao)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(avaliacao);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(avaliacao);
+        }
+
+        // GET: Avaliacaos/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var avaliacao = await _context.Avaliacaos.FindAsync(id);
+            if (avaliacao == null)
+            {
+                return NotFound();
+            }
+            return View(avaliacao);
+        }
+
+        // POST: Avaliacaos/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,UsuariosId,CardapioId")] Avaliacao avaliacao)
+        {
+            if (id != avaliacao.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(avaliacao);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!AvaliacaoExists(avaliacao.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(avaliacao);
+        }
+
+        // GET: Avaliacaos/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var avaliacao = await _context.Avaliacaos
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (avaliacao == null)
+            {
+                return NotFound();
+            }
+
+            return View(avaliacao);
+        }
+
+        // POST: Avaliacaos/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var avaliacao = await _context.Avaliacaos.FindAsync(id);
+            if (avaliacao != null)
+            {
+                _context.Avaliacaos.Remove(avaliacao);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool AvaliacaoExists(int id)
+        {
+            return _context.Avaliacaos.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ Controllers/InsercaoMedicasController.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using MenuEduca01.Data;
+using MenuEduca01.Models;
+
+namespace MenuEduca01.Controllers
+{
+    public class InsercaoMedicasController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public InsercaoMedicasController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: InsercaoMedicas
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.InsercaoMedicas.ToListAsync());
+        }
+
+        // GET: InsercaoMedicas/Details/5
+        public async Task<IActionResult> Details(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var insercaoMedica = await _context.InsercaoMedicas
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (insercaoMedica == null)
+            {
+                return NotFound();
+            }
+
+            return View(insercaoMedica);
+        }
+
+        // GET: InsercaoMedicas/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: InsercaoMedicas/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,UsuariosId,Descricao,receitaMedica,DataCadastro,Notificacao")] InsercaoMedica insercaoMedica)
+        {
+            if (ModelState.IsValid)
+            {
+                insercaoMedica.Id = Guid.NewGuid();
+                _context.Add(insercaoMedica);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(insercaoMedica);
+        }
+
+        // GET: InsercaoMedicas/Edit/5
+        public async Task<IActionResult> Edit(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var insercaoMedica = await _context.InsercaoMedicas.FindAsync(id);
+            if (insercaoMedica == null)
+            {
+                return NotFound();
+            }
+            return View(insercaoMedica);
+        }
+
+        // POST: InsercaoMedicas/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Guid id, [Bind("Id,UsuariosId,Descricao,receitaMedica,DataCadastro,Notificacao")] InsercaoMedica insercaoMedica)
+        {
+            if (id != insercaoMedica.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(insercaoMedica);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!InsercaoMedicaExists(insercaoMedica.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(insercaoMedica);
+        }
+
+        // GET: InsercaoMedicas/Delete/5
+        public async Task<IActionResult> Delete(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var insercaoMedica = await _context.InsercaoMedicas
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (insercaoMedica == null)
+            {
+                return NotFound();
+            }
+
+            return View(insercaoMedica);
+        }
+
+        // POST: InsercaoMedicas/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(Guid id)
+        {
+            var insercaoMedica = await _context.InsercaoMedicas.FindAsync(id);
+            if (insercaoMedica != null)
+            {
+                _context.InsercaoMedicas.Remove(insercaoMedica);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool InsercaoMedicaExists(Guid id)
+        {
+            return _context.InsercaoMedicas.Any(e => e.Id == id);
+        }
+    }
+}
