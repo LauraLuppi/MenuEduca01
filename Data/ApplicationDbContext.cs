0d9b8dd46236e68caaebc51e8c5aa2a06457eff4
LauraLuppi <lauravtluppi15@gmail.com>
2025-04-25 08:26:42 -0300

    Context

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -19,11 +19,34 @@ namespace MenuEduca01.Data
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
-            base.OnModelCreating(builder);
-            builder.Entity<CadastroUsuario>().ToTable("Usuarios");
-            builder.Entity<Cardapio>().ToTable("Cardapios");
-            builder.Entity<InsercaoMedica>().ToTable("InsercaoMedicas");
-            builder.Entity<Avaliacao>().ToTable("Avaliacaos");
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<CadastroUsuario>().ToTable("CadastroUsuarios");
+            modelBuilder.Entity<Cardapio>().ToTable("Cardapios");
+            modelBuilder.Entity<InsercaoMedica>().ToTable("InsercaoMedicas");
+            modelBuilder.Entity<Avaliacao>().ToTable("Avaliacaos");
+
+            // Cadastrando as Roles padrão do Sistema 
+            Guid CadastroUsuario = Guid.NewGuid();
+            modelBuilder.Entity<IdentityRole>().HasData(
+                new IdentityRole
+                {
+                    Id = CadastroUsuario.ToString(),
+                    Name = "CadastroUsuario",
+                    NormalizedName = "CADASTROUSUARIO"
+                },
+                new IdentityRole
+                {
+                    Id = Guid.NewGuid().ToString(),
+                    Name = "Aluno",
+                    NormalizedName = "ALUNO"
+                },
+                new IdentityRole
+                {
+                    Id = Guid.NewGuid().ToString(),
+                    Name = "Nutricionista",
+                    NormalizedName = "NUTRICIONISTA"
+                }
+            );
         }
     }
 }
